270c3f550e3f30c572d77d71782608ab3f5b3951
rakibhossainraju <rakibhossianraju@gamil.com>
2026-02-16 21:18:39 +0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    // Display the prompt and ensure it appears immediately before waiting for input
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Read user input and trim trailing whitespace
+    let mut user_command = String::new();
+    io::stdin().read_line(&mut user_command).unwrap();
+    user_command = user_command.trim_end().to_string();
+    println!("{}: command not found", user_command);
 }
