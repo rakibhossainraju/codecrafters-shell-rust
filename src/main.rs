9211b5e5c62f22e3ff09352d678cfcf48b98748b
rakibhossainraju <rakibhossianraju@gamil.com>
2026-02-16 22:15:36 +0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ fn main() {
         // Display the prompt and ensure it appears immediately before waiting for input
         print_initial_prompt();
         // Read user input and trim trailing whitespace
-        let user_command = read_user_command();
-        match user_command.as_str() {
-            "exit" => break,
-            _ => {}   
+        let user_input = read_user_command();
+        match user_input.get(0) {
+            Some(command) => {
+                match command.as_str() {
+                    "exit"  => break,
+                    "echo" => {
+                        execute_echo(&user_input[1..]);
+                    }
+                    _ => {
+                        println!("{}: command not found", command);
+                    }
+                }
+            }
+            None => {
+                println!("Invalid command.");
+            }
         }
-        println!("{}: command not found", user_command);
     }
 }
 
@@ -20,9 +31,23 @@ fn print_initial_prompt() {
     io::stdout().flush().unwrap();
 }
 
-fn read_user_command() -> String {
+fn read_user_command() -> Vec<String> {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    command = command.trim_end().to_string();
     command
+        .trim_end()
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>()
+}
+
+fn execute_echo(args: &[String]) {
+    if args.len() == 0 {
+        println!("No arguments supplied.");
+    }
+    let args = args.iter()
+        .map(|s| s.to_string() + " ")
+        .collect::<String>();
+    println!("{}", args);
+
 }
