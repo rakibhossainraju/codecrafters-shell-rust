1efcd560705c7efdc0f2a81313b33f477c3962eb
rakibhossainraju <rakibhossianraju@gamil.com>
2026-02-18 00:19:17 +0600

    refactor: streamline external command execution logic

--- src/commands/executors/external.rs
+++ src/commands/executors/external.rs
@@ -3,35 +3,33 @@ use crate::utils;
 use std::os::unix::prelude::CommandExt;
 
 pub fn execute_external_command(external_cmd: ExternalCommand) {
-    // Try to resolve the command
-    if let Some(external_cmd_path) = external_cmd.path {
-        // Command found, execute it
-        let mut cmd = std::process::Command::new(external_cmd_path);
-        let args = external_cmd
-            .args
-            .as_ref()
-            .map_or(vec![], |args| args.to_vec());
-        cmd.arg0(external_cmd.name.as_str());
-        cmd.args(&args);
+    let external_cmd_path = match external_cmd.path {
+        Some(path) => path,
+        None => {
+            utils::print_command_not_found(external_cmd.name.as_str());
+            return;
+        }
+    };
+    let mut cmd = std::process::Command::new(external_cmd_path);
+    let args = external_cmd.args.unwrap_or_default();
 
-        match cmd.spawn() {
-            Ok(mut child) => {
-                // Wait for the command to finish
-                if let Ok(status) = child.wait() {
-                    if !status.success() {
-                        utils::print_exit_with_status(external_cmd.name.as_str(), status);
-                        return;
-                    }
-                } else {
-                    eprintln!("Failed to wait for command '{}'", external_cmd.name);
+    cmd.arg0(external_cmd.name.as_str());
+    cmd.args(&args);
+
+    match cmd.spawn() {
+        Ok(mut child) => {
+            // Wait for the command to finish
+            if let Ok(status) = child.wait() {
+                if !status.success() {
+                    utils::print_exit_with_status(external_cmd.name.as_str(), status);
+                    return;
                 }
-            }
-            Err(e) => {
-                utils::print_filed_to_execute(external_cmd.name.as_str(), e);
+            } else {
+                eprintln!("Failed to wait for command '{}'", external_cmd.name);
             }
         }
-    } else {
-        // Command not found
-        utils::print_command_not_found(external_cmd.name.as_str());
+        Err(e) => {
+            utils::print_filed_to_execute(external_cmd.name.as_str(), e);
+        }
     }
 }
