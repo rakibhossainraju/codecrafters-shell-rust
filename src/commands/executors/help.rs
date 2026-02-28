d7ac5889b124564e9e44d84803561a7e75d0c5bc
rakibhossainraju <rakibhossianraju@gamil.com>
2026-02-28 21:33:44 +0600

    feat: refactor help command to use a centralized help function for built-in commands

--- src/commands/executors/help.rs
+++ src/commands/executors/help.rs
@@ -1,13 +1,40 @@
 use crate::error::Result;
 use std::io::{Read, Write};
+use crate::commands::BuiltinCommands;
+
+/// Get the help text for a builtin command
+fn get_command_help(cmd: BuiltinCommands) -> &'static str {
+    match cmd {
+        BuiltinCommands::Exit => "exit     - Exit the shell",
+        BuiltinCommands::Echo => "echo     - Print text to stdout",
+        BuiltinCommands::Help => "help     - Show this help message",
+        BuiltinCommands::Type => "type     - Show information about a command",
+        BuiltinCommands::Pwd => "pwd      - Print working directory",
+        BuiltinCommands::Cd => "cd       - Change directory",
+        BuiltinCommands::Clear => "clear    - Clear the screen",
+        BuiltinCommands::History => "history  - Show command history",
+    }
+}
 
 /// Execute the help builtin command
 pub fn execute_help(_stdin: &mut dyn Read, stdout: &mut dyn Write) -> Result<()> {
     writeln!(stdout, "Available builtin commands:")?;
-    writeln!(stdout, "  exit     - Exit the shell")?;
-    writeln!(stdout, "  echo     - Print text to stdout")?;
-    writeln!(stdout, "  type     - Show information about a command")?;
-    writeln!(stdout, "  help     - Show this help message")?;
-    writeln!(stdout, "  clear    - Clears screen clearing memory")?;
+
+    // This array ensures all builtin commands are listed
+    let commands = [
+        BuiltinCommands::Exit,
+        BuiltinCommands::Echo,
+        BuiltinCommands::Help,
+        BuiltinCommands::Type,
+        BuiltinCommands::Pwd,
+        BuiltinCommands::Cd,
+        BuiltinCommands::Clear,
+        BuiltinCommands::History,
+    ];
+
+    for cmd in commands.iter() {
+        writeln!(stdout, "  {}", get_command_help(*cmd))?;
+    }
+
     Ok(())
 }
