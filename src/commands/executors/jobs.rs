755c5011d8db74db5ff40f406826c6470482e273
rakibhossainraju <rakibhossainraju.dev@gmail.com>
2026-09-01 17:08:51 +0600

    commented out the jobs message

--- src/commands/executors/jobs.rs
+++ src/commands/executors/jobs.rs
@@ -2,6 +2,6 @@ use crate::error::{Result, ShellError};
 use std::io::{Read, Write};
 
 pub fn execute_jobs(_stdin: &mut dyn Read, stdout: &mut dyn Write) -> Result<()> {
-    writeln!(stdout, "Jobs are not implemented yet")?;
+    // Jobs are not implemented yet;
     Ok(())
 }
